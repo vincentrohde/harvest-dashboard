2771496ae7cfe7a9cb3d5fd5859251f93c2824b3
Vincent Rohde <vincent.rohde@icloud.com>
2019-10-12 18:20:30 +0200

    improve re-render behavior for CategoriesOverview

--- src/components/CategoriesOverview/CategoriesOverview.js
+++ src/components/CategoriesOverview/CategoriesOverview.js
@@ -1,15 +1,67 @@
-import React, { useEffect } from 'react';
+import React, { Component } from 'react';
 import Chart from 'chart.js';
 import Utils from "../../utils/utils";
 
 import style from './CategoriesOverview.scss';
 
-const CategoriesOverview = (props) => {
-    useEffect(() => {
+class CategoriesOverview extends Component {
+    constructor (props) {
+        super();
+        this.props = props;
+    }
+
+    shouldComponentUpdate (nextProps) {
+        const isEqualProps = (prevProps, newProps) => {
+            const detectedHoursDifference = (prevList, newList) => {
+                prevList.forEach((prevItem) => {
+                    newList.forEach((newItem) => {
+                        if (prevItem.category === newItem.category) {
+                            const convertedPrevHours = prevItem.hours.toFixed(0);
+                            const convertedNewHours = newItem.hours.toFixed(0);
+
+                            if (convertedNewHours !== convertedPrevHours) {
+                                return true;
+                            }
+                        }
+                    });
+                });
+
+                return false;
+            };
+            const detectedCategoryListChange = (prevList, newList) => {
+                if (typeof prevList === 'undefined' || prevList === null) {
+                    return true;
+                }
+
+                if (prevList.length < newList.length
+                    || prevList.length > newList.length)
+                {
+                    return true;
+                }
+
+                return false;
+            };
+
+            let prevInformation = prevProps.information;
+            let newInformation = newProps.information;
+
+            const categoryListChanged = detectedCategoryListChange(prevInformation, newInformation);
+
+            if (categoryListChanged) {
+                return true;
+            }
+
+            return detectedHoursDifference(prevInformation, newInformation);
+
+        };
+
+        return isEqualProps(this.props, nextProps);
+    }
+
+    componentDidUpdate () {
         const colorsArray = [
             '#f54291', '#ddb6c6', '#4baea0', '#b6e6bd', '#6bc5d2', '#d2fafb', '#ff935c', '#ef4b4b'
         ];
-
         const prepareDataForChart = (entries) => {
             entries = Utils.sortObjectsArray(true, entries, 'hours');
             const categories = entries.map(entry => entry.category);
@@ -20,9 +72,9 @@ const CategoriesOverview = (props) => {
             }
         };
 
-        if (props.information) {
+        if (this.props.information) {
             const chartContainer = document.querySelector('.chart-canvas');
-            const { information } = props;
+            const { information } = this.props;
             const { hours, categories } = prepareDataForChart(information);
 
             const data = {
@@ -49,17 +101,19 @@ const CategoriesOverview = (props) => {
                 data: data
             });
         }
-    });
+    }
 
-    return (
-        <div className="CategoriesOverview tab-container">
-            <h2 className="title">Categories</h2>
-            <p className="caption">by hours</p>
-            <div className="chart-container">
-                <canvas className="chart-canvas"></canvas>
+    render () {
+        return (
+            <div className="CategoriesOverview tab-container">
+                <h2 className="title">Categories</h2>
+                <p className="caption">by hours</p>
+                <div className="chart-container">
+                    <canvas className="chart-canvas"></canvas>
+                </div>
             </div>
-        </div>
-    );
-}
+        );
+    };
+};
 
 export default CategoriesOverview;
