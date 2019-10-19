72b5353b8e6864d7ebbba2cdbb5425cfb78ede21
Vincent Rohde <vincent.rohde@icloud.com>
2019-10-19 22:22:36 +0200

    add active selectors to store

---
+++ src/stores/selectors/index.js
@@ -0,0 +1,14 @@
+import { createSelector } from 'reselect';
+
+const activeTasksSelector = (state) => state.tasks.tasksActive;
+const activeProjectsSelector = (state) => state.projects.projectsActive;
+
+export const editFormOptionsSelector = createSelector(
+    [ activeTasksSelector, activeProjectsSelector],
+    (activeTasksSelector, activeProjectsSelector) => {
+        return {
+            activeTasksSelector,
+            activeProjectsSelector
+        }
+    }
+)
