75726d2629826d8188cd190a21ad65d7e64ed8dd
Vincent Rohde <vincent.rohde@icloud.com>
2019-10-27 22:39:33 +0100

    add filters: actions, reducers, selectors; add filters to store

---
+++ src/stores/reducers/filters.js
@@ -0,0 +1,25 @@
+import {
+    SET_FILTERS,
+    UPDATE_DATE_RANGE
+} from '../actions/filters';
+
+export const filters = (state = {}, action) => {
+    let dateRange;
+    switch (action.type) {
+        case SET_FILTERS:
+            return {
+                ...state,
+                ...action.payload
+            };
+        case UPDATE_DATE_RANGE:
+            dateRange = action.payload;
+            return {
+                ...state,
+                dateRange
+            };
+        default:
+            return {
+                ...state
+            }
+    }
+};
