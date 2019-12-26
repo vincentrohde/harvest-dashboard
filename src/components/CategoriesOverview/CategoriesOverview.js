0f7d5bdd2f0c271a7ed211afea57b3cb8f45ebb1
Vincent Rohde <vincent.rohde@icloud.com>
2019-12-26 14:17:58 +0100

    refactor CategoriesOverview component

--- src/components/CategoriesOverview/CategoriesOverview.js
+++ src/components/CategoriesOverview/CategoriesOverview.js
@@ -11,71 +11,76 @@ class CategoriesOverview extends Component {
     }
 
     shouldComponentUpdate (nextProps) {
-        const isEqualProps = (prevProps, newProps) => {
-            const detectedHoursDifference = (prevList, newList) => {
-                prevList.forEach((prevItem) => {
-                    newList.forEach((newItem) => {
-                        if (prevItem.category === newItem.category) {
-                            const convertedPrevHours = prevItem.hours.toFixed(0);
-                            const convertedNewHours = newItem.hours.toFixed(0);
-
-                            if (convertedNewHours !== convertedPrevHours) {
-                                return true;
-                            }
-                        }
-                    });
-                });
-
-                return false;
-            };
-            const detectedCategoryListChange = (prevList, newList) => {
-                if (typeof prevList === 'undefined' || prevList === null) {
-                    return true;
-                }
-
-                if (prevList.length < newList.length
-                    || prevList.length > newList.length)
-                {
-                    return true;
-                }
-
-                return false;
-            };
-
-            let prevInformation = prevProps.information;
-            let newInformation = newProps.information;
-
-            const categoryListChanged = detectedCategoryListChange(prevInformation, newInformation);
-
-            if (categoryListChanged) {
-                return true;
-            }
-
-            return detectedHoursDifference(prevInformation, newInformation);
-
-        };
-
-        return isEqualProps(this.props, nextProps);
+        return this.isEqualProps(this.props, nextProps);
     }
+
+    prepareDataForChart (entries) {
+        entries = Utils.sortObjectsArray(true, entries, 'hours');
+        const categories = entries.map(entry => entry.category);
+        const hours = entries.map(entry => Math.round(entry.hours * 100) / 100);
+        return {
+            categories,
+            hours
+        }
+    }
+
+    isPropsHoursDifference (prevList, newList) {
+        let isPropsHoursDifference = false;
+
+        prevList.forEach((prevItem) => {
+            newList.forEach((newItem) => {
+                if (prevItem.category === newItem.category) {
+                    const convertedPrevHours = prevItem.hours.toFixed(0);
+                    const convertedNewHours = newItem.hours.toFixed(0);
+
+                    if (convertedNewHours !== convertedPrevHours) {
+                        isPropsHoursDifference = true;
+                    }
+                }
+            });
+        });
+
+        return isPropsHoursDifference;
+    };
+
+    detectedCategoryListChange (prevList, newList) {
+        if (typeof prevList === 'undefined' || prevList === null) {
+            return true;
+        }
+
+        if (prevList.length < newList.length
+            || prevList.length > newList.length)
+        {
+            return true;
+        }
+
+        return false;
+    };
+
+    isEqualProps = (prevProps, newProps) => {
+
+        let prevInformation = prevProps.information;
+        let newInformation = newProps.information;
+
+        const categoryListChanged = this.detectedCategoryListChange(prevInformation, newInformation);
+
+        if (categoryListChanged) {
+            return true;
+        }
+
+        return this.isPropsHoursDifference(prevInformation, newInformation);
+
+    };
 
     componentDidUpdate () {
         const colorsArray = [
             '#f54291', '#ddb6c6', '#4baea0', '#b6e6bd', '#6bc5d2', '#d2fafb', '#ff935c', '#ef4b4b'
         ];
-        const prepareDataForChart = (entries) => {
-            entries = Utils.sortObjectsArray(true, entries, 'hours');
-            const categories = entries.map(entry => entry.category);
-            const hours = entries.map(entry => Math.round(entry.hours * 100) / 100);
-            return {
-                categories,
-                hours
-            }
-        };
 
         if (this.props.information) {
             const chartContainer = document.querySelector('.chart-canvas');
             const { information } = this.props;
-            const { hours, categories } = prepareDataForChart(information);
+            const { hours, categories } = this.prepareDataForChart(information);
 
             const data = {
                 datasets: [{
