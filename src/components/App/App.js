4efbadf3bd754b1d97a5f6b6c09fade901b4aaf1
Vincent Rohde <vincent.rohde@icloud.com>
2019-12-30 00:20:52 +0100

    refactor App to stateless component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,27 +1,19 @@
-import React, { Component } from 'react';
-
+import React from 'react';
 import { Provider } from 'react-redux';
 import store from '../../stores/store';
+import Entries from '../Entries/Entries';
 
 import style from './App.scss';
 
-import Entries from '../Entries/Entries';
-
-class App extends Component {
-    constructor () {
-        super();
-    }
-
-    render () {
-        return (
-            <Provider store={store}>
-                <div className="App">
-                    <h1 className="app-title">Harvest Dashboard</h1>
-                    <Entries />
-                </div>
-            </Provider>
-        )
-    }
+const App = () => {
+    return (
+        <Provider store={store}>
+            <div className="App">
+                <h1 className="app-title">Harvest Dashboard</h1>
+                <Entries />
+            </div>
+        </Provider>
+    )
 };
 
 export default App;
