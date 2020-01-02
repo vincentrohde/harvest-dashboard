3fe604008e2e28742e4a1fa7a0b7a86394f15e4e
Vincent Rohde <vincent.rohde@icloud.com>
2020-01-02 23:29:49 +0100

    add Entries code to ObjectHelper method isPropertyDifferentFromOldObject

--- src/helpers/ObjectHelper/ObjectHelper.js
+++ src/helpers/ObjectHelper/ObjectHelper.js
@@ -8,6 +8,25 @@ function sortObjectsArray (isDescending, list, selector) {
     });
 };
 
+function isPropertyDifferentFromOldObject (selector, oldObject, newObject) {
+    if (newObject[selector]) {
+        const oldProperty = oldObject[selector];
+        const newProperty = newObject[selector];
+
+        const oldPropertyJSON = JSON.stringify(oldProperty);
+        const newPropertyJSON = JSON.stringify(newProperty);
+
+        if (oldPropertyJSON !== newPropertyJSON) {
+            return newProperty;
+        }
+
+        return false;
+    }
+
+    return false;
+};
+
 export {
-    sortObjectsArray
+    sortObjectsArray,
+    isPropertyDifferentFromOldObject
 }
