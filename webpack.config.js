b5ea1ebda665708a423fcf650fb9bb5384596b99
Vincent Rohde <vincent.rohde@icloud.com>
2020-06-07 16:37:58 +0200

    add early express+webpack implementation

--- webpack.config.js
+++ webpack.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     output:{
         path: path.join(__dirname, '/dist'),
-        filename: 'bundle.js'
+        filename: '[name].js'
     },
     module: {
         rules: [
