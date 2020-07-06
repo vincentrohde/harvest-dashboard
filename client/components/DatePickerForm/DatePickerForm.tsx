b0b5cf6fa86b744654c92e66895f20c63fec9473
Vincent Rohde <vincent.rohde@icloud.com>
2020-07-07 00:36:22 +0200

    convert DatePickerForm

--- client/components/DatePickerForm/DatePickerForm.tsx
+++ client/components/DatePickerForm/DatePickerForm.tsx
@@ -1,188 +1,207 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+// Typescript
+
+import { FiltersInterface } from '../../../interfaces/Filters';
+
+// Libs
+
+import React, { useEffect, useState } from 'react';
 import { Form, Grid, Select } from 'semantic-ui-react';
 import moment from 'moment';
-import { timeService } from '../../lib/TimeService/TimeService';
+import { connect } from 'react-redux';
 import { DatesRangeInput} from 'semantic-ui-calendar-react';
+
+// Services
+
+import { timeService } from '../../lib/TimeService/TimeService';
+
+// Redux
+
 import { updateDateRange } from '../../stores/actions/filters';
 import { dateRangeFilterSelector } from '../../stores/selectors/filters';
 
-import style from './DatePickerForm.scss';
+// Stylings
 
-class DatePickerForm extends Component {
-    constructor (props) {
-        super();
-        this.props = props;
-        this.datePicker = false;
-        this.ranges = [
-            {
-                key: '1',
-                value: 'today',
-                text: 'Today',
-            },
-            {
-                key: '2',
-                value: 'one-week',
-                text: 'Last Week',
-            },
-            {
-                key: '3',
-                value: 'one-month',
-                text: 'Last Month',
-            },
-            {
-                key: '4',
-                value: 'one-year',
-                text: 'Last Year',
-            }
-        ];
+import './DatePickerForm.scss';
 
-        this.state = {
-            dateRange: this.props.dateRange || [moment().format('YYYY-MM-DD')],
-            preset: ''
-        };
+interface DatePickerFormProps {
+    dateRange: FiltersInterface['dateRange'];
+    updateDateRange: Function;
+}
+
+const ranges = [
+    {
+        key: '1',
+        value: 'today',
+        text: 'Today',
+    },
+    {
+        key: '2',
+        value: 'one-week',
+        text: 'Last Week',
+    },
+    {
+        key: '3',
+        value: 'one-month',
+        text: 'Last Month',
+    },
+    {
+        key: '4',
+        value: 'one-year',
+        text: 'Last Year',
     }
+];
 
-    handleChange (event, {name, value}) {
-        let dates;
-        let stateCallback;
-        if (value.length) {
-            dates = this.getArrayFromDateRangeInput(value);
-            dates = dates.map(item => timeService.ddMMYYYYToISO8601(item));
-            stateCallback = () => {
-                this.props.updateDateRange(this.state.dateRange)
-            };
-        } else {
-            dates = value;
-            stateCallback = () => {};
-        }
+const DatePickerForm = ({ dateRange, updateDateRange }: DatePickerFormProps) => {
+    let datePicker: DatesRangeInput | null;
+    const currentDay = moment().format('DD-MM-YYYY');
+    const [preset, setPreset] = useState('');
 
-        this.setState({
-            ...this.state,
-            preset: this.setPreset ? this.state.preset : '',
-            [name]: dates
-        }, () => {
-            stateCallback();
-        });
+    const cleanDateInputFormat = (dateList: RegExpMatchArray): string[] => {
+        const hyphenRegex = /-/g;
+        return dateList.map(item => item.replace(hyphenRegex, '.'));
+    };
 
-        this.setPreset = false;
-    }
-
-    getArrayFromDateRangeInput (input) {
-        const cleanDateInputFormat = (dateList) => {
-            const hyphenRegex = /-/g;
-            return dateList.map(item => item.replace(hyphenRegex, '.'));
-        };
-
+    const getArrayFromDateRangeInput = (input: string) => {
         const dateRegex = /(([0-9]{2})(-)([0-9]{2})(-)([0-9]{4}))/g;
         const matches = input.match(dateRegex);
+
+        if (matches === null) return [];
 
         return cleanDateInputFormat(matches);
     }
 
-    getDateRangeValue () {
-        if (this.state.dateRange.length) {
-            let dateRange = this.state.dateRange;
-            dateRange = dateRange.map(item => timeService.iso8601ToDDMMYYY(item));
+    const handleChange = (_event: any, { name, value }: { name: string; value: string; }) => {
+        if (name === 'preset') {
+            setPreset(value);
+            return;
+        }
 
-            if (dateRange.length >= 2) {
-                return `${dateRange[0]} - ${dateRange[1]}`;
+        if (name === 'dateRange') {
+            let dates;
+
+            if (value.length) {
+                dates = getArrayFromDateRangeInput(value);
+                dates = dates.map(item => timeService.ddMMYYYYToISO8601(item));
+            } else {
+                dates = value;
             }
 
-            return `${dateRange[0]}`
-        } else {
-            return this.state.dateRange
+            updateDateRange(dates);
+            return;
         }
     }
 
-    setInputToCurrentDate (ev, {value}) {
-        const event = new Event('input', { bubbles: true });
-        const input = this.datePicker.inputNode;
-        const currentDay = moment().format('DD-MM-YYYY');
-        let inputValue, oneWeekAgo, oneMonthAgo, oneYearAgo;
-        this.setPreset = true;
+    const setDatePicker = (element: DatesRangeInput) => {
+        datePicker = element;
+    };
 
-        this.setState({
-            ...this.state,
-            preset: value
-        }, () => {
-            switch (value) {
-                case 'today':
-                    inputValue = `${currentDay} - `;
-                    break;
-                case 'one-week':
-                    oneWeekAgo = moment().subtract(6,'days').format('DD-MM-YYYY');
-                    inputValue = `${oneWeekAgo} - ${currentDay}`;
-                    break;
-                case 'one-month':
-                    oneMonthAgo = moment().subtract(29,'days').format('DD-MM-YYYY');
-                    inputValue = `${oneMonthAgo} - ${currentDay}`;
-                    break;
-                case 'one-year':
-                    oneYearAgo = moment().subtract(364,'days').format('DD-MM-YYYY');
-                    inputValue = `${oneYearAgo} - ${currentDay}`;
-                    break;
-                default:
-                    return;
-            }
+    const getDateRangeValue = () => {
+        if (!dateRange.length) return '';
 
-            this.setNativeValue(input, inputValue);
-            input.dispatchEvent(event);
-        });
+        const convertedDateRange = dateRange.map((item: any) => timeService.iso8601ToDDMMYYY(item));
+
+        if (convertedDateRange.length >= 2) {
+            return `${convertedDateRange[0]} - ${convertedDateRange[1]}`;
+        }
+
+        return `${convertedDateRange[0]}`;
     }
 
     // todo: what is this doing?
-    setNativeValue (element, value) {
+    const setNativeValue = (element: any, value: string) => {
+        // @ts-ignore
         const valueSetter = Object.getOwnPropertyDescriptor(element, 'value').set;
         const prototype = Object.getPrototypeOf(element);
+        // @ts-ignore
         const prototypeValueSetter = Object.getOwnPropertyDescriptor(prototype, 'value').set;
 
         if (valueSetter && valueSetter !== prototypeValueSetter) {
+            // @ts-ignore
             prototypeValueSetter.call(element, value);
         } else {
+            // @ts-ignore
             valueSetter.call(element, value);
         }
     };
 
-    render () {
-        return (
-            <Form
-                className="DatePickerForm"
-            >
-                <Grid>
-                    <Grid.Column width={10}>
-                        <DatesRangeInput
-                            className="date-picker"
-                            name="dateRange"
-                            label={{ children: "Date Range", htmlFor: "form-select-control-task" }}
-                            placeholder="From - To"
-                            value={this.getDateRangeValue()}
-                            iconPosition="left"
-                            onChange={this.handleChange.bind(this)}
-                            ref={(element) => { this.datePicker = element; }}
-                        />
-                    </Grid.Column>
-                    <Grid.Column width={6}>
-                        <Form.Field
-                            control={Select}
-                            label={{ children: "Preset", htmlFor: "form-select-control-task" }}
-                            search
-                            searchInput={{ id: "form-select-control-task" }}
-                            options={this.ranges}
-                            placeholder="Preset"
-                            name="preset"
-                            clearable
-                            onChange={this.setInputToCurrentDate.bind(this)}
-                            value={this.state.preset}
-                        />
-                    </Grid.Column>
-                </Grid>
-            </Form>
-        )
+    const getEvent = (name: string) => {
+        return new Event(name, { bubbles: true });
+    };
+
+    const dispatchCustomEvent = (element: Node, event: Event) => {
+        element.dispatchEvent(event);
     }
+
+    const getInputFromPreset = () => {
+        let oneWeekAgo, oneMonthAgo, oneYearAgo;
+        switch (preset) {
+            case 'today':
+                return `${currentDay} - `;
+            case 'one-week':
+                oneWeekAgo = timeService.getDateFromDaysAgo(6);
+                return `${oneWeekAgo} - ${currentDay}`;
+            case 'one-month':
+                oneMonthAgo = timeService.getDateFromDaysAgo(29);
+                return `${oneMonthAgo} - ${currentDay}`;
+            case 'one-year':
+                oneYearAgo = timeService.getDateFromDaysAgo(364);
+                return `${oneYearAgo} - ${currentDay}`;
+            default:
+                return '';
+        }
+    };
+
+    useEffect(() => {
+        const inputEvent = getEvent('input');
+        // @ts-ignore
+        const input = datePicker.inputNode;
+        const inputValue = getInputFromPreset();
+
+        setNativeValue(input, inputValue);
+        dispatchCustomEvent(input, inputEvent);
+    }, [preset]);
+
+    return (
+        <Form className="DatePickerForm">
+            <Grid>
+                <Grid.Column width={10}>
+                    <DatesRangeInput
+                        className="date-picker"
+                        name="dateRange"
+                        label={{
+                            children: "Date Range",
+                            htmlFor: "form-select-control-task" }}
+                        placeholder="From - To"
+                        value={getDateRangeValue()}
+                        iconPosition="left"
+                        onChange={handleChange}
+                        ref={setDatePicker}
+                    />
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <Form.Field
+                        control={Select}
+                        label={{
+                            children: "Preset",
+                            htmlFor: "form-select-control-task" }}
+                        search
+                        searchInput={{ id: "form-select-control-task" }}
+                        options={ranges}
+                        placeholder="Preset"
+                        name="preset"
+                        clearable
+                        onChange={handleChange}
+                        value={preset}
+                    />
+                </Grid.Column>
+            </Grid>
+        </Form>
+    )
 }
 
-const mapStateToProps = state => {
+const mapStateToProps = (state: any): {
+    dateRange: FiltersInterface['dateRange'];
+} => {
     return {
         dateRange: dateRangeFilterSelector(state)
     }
