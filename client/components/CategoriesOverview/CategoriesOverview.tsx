80fe30f0db5c985290f65ea431f52889f9158bee
Vincent Rohde <vincent.rohde@icloud.com>
2020-07-07 00:25:59 +0200

    convert CategoriesOverview

--- client/components/CategoriesOverview/CategoriesOverview.tsx
+++ client/components/CategoriesOverview/CategoriesOverview.tsx
@@ -1,101 +1,74 @@
-import React, { Component } from 'react';
+// Typescript
+
+import { timeEntriesType } from '../../../interfaces/TimeEntry';
+
+// Libs
+
+import React, { useState, useEffect } from 'react';
 import Chart from 'chart.js';
-import { objectService } from '../../lib/ObjectService/ObjectService';
-import { timeEntriesSelector } from '../../stores/selectors/timeEntries';
+
+// Redux
+
 import { connect } from 'react-redux';
 
-import style from './CategoriesOverview.scss';
+// Hooks
 
-class CategoriesOverview extends Component {
-    constructor (props) {
-        super();
-        this.props = props;
-        this.colorsArray = [
-            '#f54291', '#ddb6c6', '#4baea0', '#b6e6bd', '#6bc5d2', '#d2fafb', '#ff935c', '#ef4b4b'
-        ];
+import { usePrevious } from '../../hooks/usePrevious';
+
+// Services
+
+import { objectService } from '../../lib/ObjectService/ObjectService';
+import { timeEntriesSelector } from '../../stores/selectors/timeEntries';
+
+// Variables
+
+import { dataColors } from '../../variables/colors'
+
+// Stylings
+
+import './CategoriesOverview.scss';
+
+type hoursByCategoryType = HoursByCategoryItemInterface[];
+
+interface HoursByCategoryItemInterface {
+    category: string;
+    hours: number;
+}
+
+interface CategoriesOverviewProps {
+    timeEntries: timeEntriesType;
+}
+
+const CategoriesOverview = ({ timeEntries }: CategoriesOverviewProps) => {
+    const colorsArray = dataColors;
+    const [hoursByCategory, setHoursByCategory] = useState<hoursByCategoryType>([]);
+
+    const prevHoursByCategory = usePrevious(hoursByCategory);
+
+    const getHoursByTwoDecimals = (entries: hoursByCategoryType) => {
+        return entries.map((entry) =>
+            Math.round(entry.hours * 100) / 100);
     }
 
-    shouldComponentUpdate (nextProps) {
-        const nextData = this.getHoursByCategory(nextProps.timeEntries);
-        return this.isEqualProps(this.hoursByCategory, nextData);
+    const getCategoriesFromEntries = (entries: hoursByCategoryType) => {
+        return entries.map(entry => entry.category);
     }
 
-    componentDidMount () {
-        this.setChart();
-    }
-
-    componentDidUpdate () {
-        this.setChart();
-    }
-
-    convertRawDataForChart (entries) {
+    const convertRawDataForChart = (entries: hoursByCategoryType) => {
         entries = objectService.sortObjectsArray(true, entries, 'hours');
-        const categories = this.getCategoriesFromEntries(entries);
-        const hours = this.getHoursByTwoDecimals(entries);
+        const categories = getCategoriesFromEntries(entries);
+        const hours = getHoursByTwoDecimals(entries);
         return {
             categories,
             hours
         }
     }
 
-    getHoursByTwoDecimals (entries) {
-        return entries.map(entry => Math.round(entry.hours * 100) / 100);
-    }
-
-    getCategoriesFromEntries (entries) {
-        return entries.map(entry => entry.category);
-    }
-
-    isPropsHoursDifference (prevList, newList) {
-        let isPropsHoursDifference = false;
-
-        if (typeof newList === 'undefined' || newList.length === 0) {
-            return isPropsHoursDifference;
-        }
-
-        prevList.forEach((prevItem) => {
-            newList.forEach((newItem) => {
-                if (prevItem.category === newItem.category) {
-                    const convertedPrevHours = Number(prevItem.hours).toFixed(0);
-                    const convertedNewHours = Number(newItem.hours).toFixed(0);
-
-                    if (convertedNewHours !== convertedPrevHours) {
-                        isPropsHoursDifference = true;
-                    }
-                }
-            });
-        });
-
-        return isPropsHoursDifference;
-    }
-
-    isCategoryListChange (prevList, newList) {
-        if (typeof prevList === 'undefined' || prevList === null || prevList.length === 0) {
-            return true;
-        }
-
-        if ((prevList.length < newList.length) || (prevList.length > newList.length)) {
-            return true;
-        }
-
-        return false;
-    }
-
-    isEqualProps (prevProps, newProps) {
-        const isCategoryListChange = this.isCategoryListChange(prevProps, newProps);
-
-        if (isCategoryListChange) {
-            return true;
-        }
-
-        return this.isPropsHoursDifference(prevProps, newProps);
-    }
-
-    getChartDataObject (data, labels) {
+    const getChartDataObject = (data: number[], labels: string[]) => {
         return {
             datasets: [{
                 label: '',
-                backgroundColor: this.colorsArray,
+                backgroundColor: colorsArray,
                 borderWidth: '0',
                 data
             }],
@@ -112,42 +85,22 @@ class CategoriesOverview extends Component {
         };
     }
 
-    setChart () {
-        if (this.hoursByCategory) {
-            const chartContainer = document.querySelector('.chart-canvas');
-            const { hours, categories } = this.convertRawDataForChart(this.hoursByCategory);
-            const data = this.getChartDataObject(hours, categories);
+    const setChart = (chartData: hoursByCategoryType) => {
+        if (chartData.length) {
+            // const chartContainer = document.querySelector('.chart-canvas');
+            const { hours, categories } = convertRawDataForChart(chartData);
+            const data = getChartDataObject(hours, categories);
 
-            const chart = new Chart(chartContainer, {
+            new Chart('.chart-canvas', {
                 type: 'doughnut',
+                // @ts-ignore
                 data
             });
         }
     }
 
-    getHoursByCategory (entries) {
-        const getHoursByCategory = (entries) => {
-            const categoriesOnlyList = entries.map(entry => entry.category);
-            const uniqueCategories = [...new Set(categoriesOnlyList)];
-            let hoursByCategoryList = uniqueCategories.map((category) => {
-                return {
-                    category,
-                    hours: 0
-                }
-            });
-
-            entries.forEach(entry => {
-                hoursByCategoryList.forEach((category) => {
-                    if (category.category == entry.category) {
-                        category.hours += entry.hours;
-                    }
-                });
-            });
-
-            return hoursByCategoryList;
-        };
-
-        let filteredEntries = [];
+    const getEntriesWithCategory = (entries: timeEntriesType) => {
+        const filteredEntries: hoursByCategoryType = [];
 
         entries.forEach((entry) => {
             filteredEntries.push({
@@ -156,30 +109,97 @@ class CategoriesOverview extends Component {
             });
         });
 
-        return getHoursByCategory(filteredEntries);
+        return filteredEntries;
     }
 
-    render () {
-        if (this.props.timeEntries) {
-            this.hoursByCategory = this.getHoursByCategory(this.props.timeEntries);
+    const getUniqueCategoriesFromEntries = (entries: hoursByCategoryType) => {
+        const categoriesOnlyList = entries.map(entry => entry.category);
+        return [...new Set(categoriesOnlyList)];
+    };
+
+    const getHoursByCategoryList = (categories: string[]): hoursByCategoryType => {
+        return categories.map((category) => {
+            return {
+                category,
+                hours: 0
+            }
+        });
+    }
+
+    const getHoursByCategory = (entries: timeEntriesType): hoursByCategoryType => {
+        const entriesWithCategory = getEntriesWithCategory(entries);
+        const uniqueCategories = getUniqueCategoriesFromEntries(entriesWithCategory);
+
+        let hoursByCategoryList = getHoursByCategoryList(uniqueCategories);
+
+        entriesWithCategory.forEach(entry => {
+            hoursByCategoryList.forEach((category) => {
+                if (category.category == entry.category) {
+                    category.hours += entry.hours;
+                }
+            });
+        });
+
+        return hoursByCategoryList;
+    };
+
+    const isHoursDifference = (prevList: hoursByCategoryType, nextList: hoursByCategoryType) => {
+        let isPropsHoursDifference = false;
+
+        if (typeof nextList === 'undefined' || nextList.length === 0) {
+            return isPropsHoursDifference;
         }
 
-        return (
-            <div className="CategoriesOverview tab-container">
-                <h2 className="title">Categories</h2>
-                <p className="caption">by hours</p>
-                <div className="chart-container">
-                    <canvas className="chart-canvas"></canvas>
-                </div>
-            </div>
-        );
+        for (let prevItem of prevList) {
+            if (isPropsHoursDifference) break;
+
+            for (let nextItem of nextList) {
+                if (prevItem.category === nextItem.category) {
+                    const convertedPrevHours = Number(prevItem.hours)
+                        .toFixed(0);
+                    const convertedNewHours = Number(nextItem.hours)
+                        .toFixed(0);
+
+                    if (convertedNewHours !== convertedPrevHours) {
+                        isPropsHoursDifference = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        return isPropsHoursDifference;
     }
+
+    // if timeentries different from old, then new hoursByCategory
+    useEffect(() => {
+        setHoursByCategory(getHoursByCategory(timeEntries));
+    }, [timeEntries]);
+
+    // if hoursByCategory different then new setchart
+    useEffect(() => {
+        if (!isHoursDifference(prevHoursByCategory, hoursByCategory)) return;
+
+        setChart(hoursByCategory);
+    }, [hoursByCategory]);
+
+    return (
+        <div className="CategoriesOverview tab-container">
+            <h2 className="title">Categories</h2>
+            <p className="caption">by hours</p>
+            <div className="chart-container">
+                <canvas className="chart-canvas" />
+            </div>
+        </div>
+    );
 };
 
-const mapStateToProps = state => {
+const mapStateToProps = (state: any): any => {
     return {
         timeEntries: timeEntriesSelector(state)
     }
 };
 
-export default connect(mapStateToProps, null)(CategoriesOverview);
+export default connect(mapStateToProps, null)(
+    React.memo(CategoriesOverview)
+);
