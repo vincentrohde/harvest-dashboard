6a79bdc1115413aea8d049d2e62a1f58e8b3eb3a
Vincent Rohde <vincent.rohde@icloud.com>
2020-07-12 14:53:18 +0200

    convert TimeService

--- client/lib/TimeService/TimeService.ts
+++ client/lib/TimeService/TimeService.ts
@@ -1,30 +1,30 @@
 import moment from 'moment';
 
 class TimeService {
-    hoursToHoursAndMinutes (hours) {
-        const totalMinutes = hours * 60;
+    hoursToHoursAndMinutes (hours: number | string): string {
+        const totalMinutes = Number(hours) * 60;
 
-        const convertedHours = Math.floor(totalMinutes / 60);
-        let convertedMinutes = Math.round(totalMinutes % 60);
+        const convertedHours = String(Math.floor(totalMinutes / 60));
+        let convertedMinutes = String(Math.round(totalMinutes % 60));
 
         if (convertedMinutes.toString().length == 1) {
-            convertedMinutes = '0' + convertedMinutes;
+            convertedMinutes = '0' + convertedMinutes.toString();
         }
 
         return `${convertedHours}:${convertedMinutes}`;
     }
 
-    hoursAndMinutesToHours (time) {
-        time = time.toString();
-        const timeComponents = time.split(':');
-        const dec = parseInt((timeComponents[1]/6)*10, 10);
+    hoursAndMinutesToHours (time: string): number {
+        const timeComponents = time.split(':').map(i=>Number(i));
+        const dec = parseInt(String((timeComponents[1] / 6) * 10), 10);
 
-        const hours = parseFloat(parseInt(timeComponents[0], 10) + '.' + (dec<10?'0':'') + dec);
+        const hours = parseFloat(parseInt(String(timeComponents[0]), 10)
+            + '.' + (dec<10?'0':'') + dec);
 
         return Number(hours);
     }
 
-    iso8601ToDDMMYYY (date) {
+    iso8601ToDDMMYYY (date: string): string {
         const isDDMMYYYY = moment(date, 'DD.MM.YYYY').format('DD.MM.YYYY') === date;
 
         if (isDDMMYYYY) {
@@ -34,7 +34,7 @@ class TimeService {
         return moment(date).format('DD.MM.YYYY');
     }
 
-    ddMMYYYYToISO8601 (date) {
+    ddMMYYYYToISO8601 (date: string): string {
         const isISO8601 = moment(date, 'YYYY-MM-DD').format('YYYY-MM-DD') === date;
 
         if (isISO8601) {
@@ -42,6 +42,14 @@ class TimeService {
         }
 
         return moment(date, 'DD.MM.YYYY').format('YYYY-MM-DD');
+    }
+
+    getDateFromDaysAgo (days: number, format: string = 'DD-MM-YYYY') {
+        return moment().subtract(days,'days').format(format);
+    }
+
+    getCurrentDate (format: string = 'YYYY-MM-DD') {
+        return moment().format(format);
     }
 }
 
