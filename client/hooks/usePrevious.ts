c65d814a9c63a8414a515d600990912cc8c4c0a4
Vincent Rohde <vincent.rohde@icloud.com>
2020-07-12 15:34:15 +0200

    convert usePrevious hook

--- client/hooks/usePrevious.ts
+++ client/hooks/usePrevious.ts
@@ -1,9 +1,10 @@
 import { useEffect, useRef } from 'react';
 
-export const usePrevious = (value) => {
-    const ref = useRef();
+export const usePrevious = <T>(value: T) => {
+    const ref = useRef<T>();
     useEffect(() => {
         ref.current = value;
     });
-    return ref.current;
+
+    return ref.current as T;
 }
