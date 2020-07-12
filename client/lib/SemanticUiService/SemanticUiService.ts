46466e0a95e4c6e9266b162d16ebecd3591bb548
Vincent Rohde <vincent.rohde@icloud.com>
2020-07-12 14:52:03 +0200

    add SemanticUiService

--- client/lib/SemanticUiService/SemanticUiService.ts
+++ client/lib/SemanticUiService/SemanticUiService.ts
@@ -1,8 +1,20 @@
 // service for common 'semantic-ui-react' tasks
+
+interface ApiData {
+    id: string;
+    name: string;
+}
+
+interface SelectOption {
+    value: string;
+    text: string;
+    key: string;
+}
+
 class SemanticUiService {
     // conversion for Select options format
-    convertDataToSelectOptions (data) {
-        return data.map(item => {
+    convertDataToSelectOptions (data: ApiData[]): SelectOption[] {
+        return data.map((item) => {
             return {
                 value: item.id,
                 text: item.name,
