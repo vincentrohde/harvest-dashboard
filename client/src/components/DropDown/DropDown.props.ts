c3cbb0014842c326caa38c473a622d1e33fc7c3b
Vincent Rohde <vincent.rohde@icloud.com>
2021-07-27 00:10:45 +0200

    extract DropDown from EditForm

--- client/src/components/DropDown/DropDown.props.ts
+++ client/src/components/DropDown/DropDown.props.ts
@@ -1,4 +1,4 @@
-import { SemanticInputProps } from '../../../../../interfaces/components/SemanticInput';
+import { SemanticInputProps } from '../../../interfaces/components/SemanticInput';
 
 export interface DropDownProps extends SemanticInputProps {
     clearable?: boolean;
