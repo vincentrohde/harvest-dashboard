3ef2fd1ac77ba2eabc5a8690de379bd1accf1cd8
Vincent Rohde <vincent.rohde@icloud.com>
2021-07-27 00:05:23 +0200

    add Preset component for DatePickerForm

---
+++ client/src/components/DatePickerForm/components/Preset/Preset.tsx
@@ -0,0 +1,27 @@
+// Libs
+import React from 'react';
+
+// Components
+import DropDown from '../../../EditForm/components/DropDown/DropDown';
+
+// Data
+import { dateRanges } from './dateRanges';
+
+// Types
+import { PresetProps } from './Preset.types';
+
+const Preset = ({ onChange, preset }: PresetProps) => <DropDown
+    label={{
+        children: "Preset",
+        htmlFor: "form-select-control-task",
+    }}
+    searchInputId={"form-select-control-task"}
+    clearable={true}
+    options={dateRanges}
+    placeholder={"Preset"}
+    name={"preset"}
+    onChange={onChange}
+    value={preset}
+/>
+
+export default Preset;
