47c55697dbcd486ce77b412fcee5897cf5a9579c
Vincent Rohde <vincent.rohde@icloud.com>
2021-07-30 12:54:16 +0200

    add TimeEntriesContainer component

---
+++ client/src/components/TimeEntriesContainer/TimeEntriesContainer.tsx
@@ -0,0 +1,38 @@
+// Libs
+import React from 'react';
+
+// Components
+import TimeEntries from '../TimeEntries/TimeEntries';
+
+// Redux
+import { connect } from 'react-redux';
+import { timeEntriesSelector } from '../../stores/selectors/timeEntries';
+
+// Hooks
+import { useTotalHoursAndMinutes } from './hooks/useTotalHoursAndMinutes/useTotalHoursAndMinutes';
+
+// Types
+import { TimeEntriesContainerProps } from './TimeEntriesContainer.types';
+import MetaDataHeader from '../MetaDataHeader/MetaDataHeader';
+
+const TimeEntriesContainer = ({ timeEntries }: TimeEntriesContainerProps) => {
+    let totalHoursAndMinutes = '0:00';
+    if (typeof timeEntries !== 'undefined') {
+        totalHoursAndMinutes = useTotalHoursAndMinutes(timeEntries);
+    }
+    return (<>
+        { typeof timeEntries !== 'undefined' && <>
+            <MetaDataHeader totalHoursAndMinutes={totalHoursAndMinutes} entriesAmount={timeEntries.length}/>
+            <TimeEntries />
+        </> }
+    </>);
+};
+
+const mapStateToProps = (state: any) => {
+    return {
+        timeEntries: timeEntriesSelector(state)
+    }
+};
+
+export default connect(mapStateToProps, null)(TimeEntriesContainer);
+
---
+++ client/src/components/TimeEntriesContainer/TimeEntriesContainer.types.ts
@@ -0,0 +1,5 @@
+import { timeEntriesType } from '../../../interfaces/TimeEntry';
+
+export interface TimeEntriesContainerProps {
+    timeEntries?: timeEntriesType;
+}
