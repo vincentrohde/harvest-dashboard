af9dea13ee55df472db667aa2e2ceb617c1775c0
Vincent Rohde <vincent.rohde@icloud.com>
2021-07-30 12:53:20 +0200

    add useTotalHoursAndMinutes hook to TimeEntriesContainer

---
+++ client/src/components/TimeEntriesContainer/hooks/useTotalHoursAndMinutes/useTotalHoursAndMinutes.ts
@@ -0,0 +1,29 @@
+// Libs
+import { useEffect, useState } from 'react';
+
+// Services
+import { timeService } from '../../../../lib/TimeService/TimeService';
+
+// Types
+import { timeEntriesType } from '../../../../../interfaces/TimeEntry';
+
+export const useTotalHoursAndMinutes = (timeEntries: timeEntriesType) => {
+    const [totalHoursAndMinutes, setTotalHoursAndMinutes] = useState('0:00');
+
+    const getTimeEntriesTotalMinutes = () => {
+        let totalMinutes = 0;
+        timeEntries.forEach(item => totalMinutes += timeService.hoursToMinutes(item.hours));
+        return totalMinutes;
+    }
+
+    const getTotalHoursAndMinutes = () => {
+        const timeEntriesTotalMinutes = getTimeEntriesTotalMinutes();
+        return timeService.minutesToHoursAndMinutes(timeEntriesTotalMinutes);
+    };
+
+    useEffect(() => {
+        setTotalHoursAndMinutes(getTotalHoursAndMinutes());
+    }, [timeEntries]);
+
+    return totalHoursAndMinutes;
+};
