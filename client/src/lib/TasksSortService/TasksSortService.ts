95f38361dce515f59f3a9abeb32b63ef44ee1d50
Vincent Rohde <vincent.rohde@icloud.com>
2021-08-09 23:29:40 +0200

    add TasksSortService

---
+++ client/src/lib/TasksSortService/TasksSortService.ts
@@ -0,0 +1,48 @@
+// Libs
+import { timeEntriesType } from '../../../interfaces/TimeEntry';
+
+// Types
+import { tasksByHours } from './TasksSortService.types';
+
+class TasksSortService {
+    getEntriesWithTask(entries: timeEntriesType): tasksByHours {
+        return entries.map(entry => ({
+            task: entry.task.name,
+            hours: entry.hours
+        }));
+    }
+
+    getUniqueTasksFromEntries(entries: tasksByHours) {
+        const tasksOnlyList = entries.map(entry => entry.task);
+        return [...new Set(tasksOnlyList)];
+    };
+
+    getTasksByHoursList (tasks: string[]): tasksByHours {
+        return tasks.map((task) => ({
+            task,
+            hours: 0
+        }));
+    };
+
+    addEntryHoursToTasks(entriesWithCategory: tasksByHours, tasksByHoursList: tasksByHours) {
+        entriesWithCategory.forEach(entry => {
+            tasksByHoursList.forEach((category) => {
+                if (category.task == entry.task) {
+                    category.hours += entry.hours;
+                }
+            });
+        });
+    }
+
+    getTasksByHours(entries: timeEntriesType): tasksByHours {
+        const entriesWithCategory = this.getEntriesWithTask(entries);
+        const uniqueCategories = this.getUniqueTasksFromEntries(entriesWithCategory);
+
+        let tasksByHoursList = this.getTasksByHoursList(uniqueCategories);
+        this.addEntryHoursToTasks(entriesWithCategory, tasksByHoursList);
+
+        return tasksByHoursList;
+    };
+}
+
+export const tasksSortService = new TasksSortService();
