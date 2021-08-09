97e36faa8112a0c0df8bb53f9d82f57a7b3e9ef4
Vincent Rohde <vincent.rohde@icloud.com>
2021-08-09 23:47:58 +0200

    update useIsNewStateDifferent

--- client/src/hooks/useIsNewStateDifferent/useIsNewStateDifferent.ts
+++ client/src/hooks/useIsNewStateDifferent/useIsNewStateDifferent.ts
@@ -1,14 +1,21 @@
-import { useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import { usePrevious } from '../usePrevious';
 
-export const useIsNewStateDifferent = <T>(state: T, callback: Function) => {
+export const useIsNewStateDifferent = <T>(state: T) => {
+    const [isNewStateDifferent, setIsNewStateDifferent] = useState(false);
     const prevState = usePrevious(state);
 
     useEffect(() => {
+        let isDifferent = false;
+
         if ((typeof prevState === 'undefined' && typeof state !== 'undefined') ||
             ((typeof prevState !== 'undefined' && typeof state !== 'undefined') &&
                 (JSON.stringify(prevState) !== JSON.stringify(state)))) {
-            callback();
+            isDifferent = true;
         }
-    });
+
+        setIsNewStateDifferent(isDifferent);
+    }, [state]);
+
+    return isNewStateDifferent;
 }
