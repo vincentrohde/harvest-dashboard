a05c5b7cc76cae58a631d7a858876b0fe461e27d
Vincent Rohde <vincent.rohde@icloud.com>
2021-08-11 19:43:01 +0200

    update Task interfaces, types

--- client/interfaces/Task.ts
+++ client/interfaces/Task.ts
@@ -3,3 +3,10 @@ import { categoriesType, CategoryInterface } from './Category';
 export type tasksType = categoriesType;
 
 export interface TaskInterface extends CategoryInterface {}
+
+export type tasksByHours = TaskByHours[];
+
+export interface TaskByHours {
+    task: string;
+    hours: number;
+}
