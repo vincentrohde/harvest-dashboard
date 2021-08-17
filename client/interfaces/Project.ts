37d6be46bf4b5269d3434fab81b8e694583f4702
Vincent Rohde <vincent.rohde@icloud.com>
2021-08-17 21:54:23 +0200

    update Project interfaces

--- client/interfaces/Project.ts
+++ client/interfaces/Project.ts
@@ -3,3 +3,10 @@ import { categoriesType, CategoryInterface } from './Category';
 export type projectsType = categoriesType;
 
 export interface ProjectInterface extends CategoryInterface {}
+
+export type projectsByHours = ProjectByHours[];
+
+export interface ProjectByHours {
+    task: string;
+    hours: number;
+}
