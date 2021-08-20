0a0a4170d4ab6f1b55b8ecb4a08e01ddd4c7e96e
Vincent Rohde <vincent.rohde@icloud.com>
2021-08-20 14:15:16 +0200

    remove getGetByHours from useGetByHours hook

--- client/src/components/DataOverview/hooks/useGetByHours/useGetByHours.ts
+++ client/src/components/DataOverview/hooks/useGetByHours/useGetByHours.ts
@@ -7,23 +7,19 @@ import { getByHours } from './useGetByHours.types';
 import { group } from '../../DataOverview.types';
 
 export const useGetByHours = (group: group) => {
-    const getGetByHours = () => {
-        let getByHours: getByHours;
+    let getByHours: getByHours;
 
-        switch (group) {
-            case 'tasks':
-                getByHours = tasksSortService.getTasksByHours.bind(tasksSortService);
-                break;
-            case 'projects':
-                getByHours = projectsSortService.getProjectsByHours.bind(projectsSortService);
-                break;
-            default:
-                getByHours = tasksSortService.getTasksByHours.bind(tasksSortService);
-                break;
-        }
-
-        return getByHours;
+    switch (group) {
+        case 'tasks':
+            getByHours = tasksSortService.getTasksByHours.bind(tasksSortService);
+            break;
+        case 'projects':
+            getByHours = projectsSortService.getProjectsByHours.bind(projectsSortService);
+            break;
+        default:
+            getByHours = tasksSortService.getTasksByHours.bind(tasksSortService);
+            break;
     }
 
-    return getGetByHours();
+    return getByHours;
 };
