7048598699ea57bfbca63bb5f021b5385247e600
Vincent Rohde <vincent.rohde@icloud.com>
2021-08-20 14:14:50 +0200

    remove state, getGroupLabel for useGroupLabel hook

--- client/src/components/DataOverview/hooks/useGroupLabel/useGroupLabel.ts
+++ client/src/components/DataOverview/hooks/useGroupLabel/useGroupLabel.ts
@@ -1,35 +1,20 @@
-// Libs
-import { useState, useEffect } from 'react';
-
 // Types
 import { groupLabel } from './useGroupLabel.types';
 import { group } from '../../DataOverview.types';
 
 export const useGroupLabel = (group: group) => {
-    const [groupLabel, setGroupLabel] = useState<groupLabel>('task');
+    let groupLabel: groupLabel;
 
-    const getGroupLabel = () => {
-        let groupLabel: groupLabel;
-
-        switch (group) {
-            case 'tasks':
-                groupLabel = 'task';
-                break;
-            case 'projects':
-                groupLabel = 'project';
-                break;
-            default:
-                groupLabel = 'task';
-        }
-
-        return groupLabel;
+    switch (group) {
+        case 'tasks':
+            groupLabel = 'task';
+            break;
+        case 'projects':
+            groupLabel = 'project';
+            break;
+        default:
+            groupLabel = 'task';
     }
-
-    useEffect(() => {
-        if (typeof group !== 'undefined') {
-            setGroupLabel(getGroupLabel());
-        }
-    }, [group]);
 
     return groupLabel;
 };
