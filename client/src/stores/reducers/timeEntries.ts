e345cb36d340070e06b945ec79f8e3bd4fc9d49b
Vincent Rohde <vincent.rohde@icloud.com>
2021-08-27 15:22:33 +0200

    update timeEntries structure for redux state

--- client/src/stores/reducers/timeEntries.ts
+++ client/src/stores/reducers/timeEntries.ts
@@ -8,47 +8,28 @@ import { TimeEntryInterface } from '@/types/TimeEntry';
 import {
     ADD_TIME_ENTRIES,
     ADD_TIME_ENTRY,
-    UPDATE_EDIT_ENTRY,
     UPDATE_TIME_ENTRY,
     DELETE_TIME_ENTRY
 } from '@/stores/actions/timeEntries';
 
-export const timeEntries = (state: any = {}, action: any) => {
+export const timeEntries = (state: TimeEntryInterface[] = [], action: any) => {
     let timeEntry: TimeEntryInterface;
     let prevTimeEntries: TimeEntryInterface[];
     switch (action.type) {
         case ADD_TIME_ENTRIES:
-            const timeEntries = action.payload;
-            return {
-                ...state,
-                timeEntries: [...timeEntries]
-            };
+            return [...action.payload];
         case ADD_TIME_ENTRY:
             timeEntry = action.payload;
-            prevTimeEntries = [...state.timeEntries];
-            return {
-                ...state,
-                timeEntries: timeEntryService.addTimeEntry(timeEntry, prevTimeEntries)
-            };
-        case UPDATE_EDIT_ENTRY:
-            return {
-                ...state,
-                editEntry: action.payload
-            };
+            prevTimeEntries = [...state];
+            return [...timeEntryService.addTimeEntry(timeEntry, prevTimeEntries)];
         case UPDATE_TIME_ENTRY:
             timeEntry = action.payload;
-            prevTimeEntries = [...state.timeEntries];
-            return {
-                ...state,
-                timeEntries: timeEntryService.updateTimeEntry(timeEntry, prevTimeEntries)
-            };
+            prevTimeEntries = [...state];
+            return [...timeEntryService.updateTimeEntry(timeEntry, prevTimeEntries)];
         case DELETE_TIME_ENTRY:
             const id = action.payload;
-            prevTimeEntries = [...state.timeEntries];
-            return {
-                ...state,
-                timeEntries: timeEntryService.deleteTimeEntry(id, prevTimeEntries)
-            }
+            prevTimeEntries = [...state];
+            return [...timeEntryService.deleteTimeEntry(id, prevTimeEntries)]
         default:
             return state
     }
--- client/src/stores/selectors/timeEntries.ts
+++ client/src/stores/selectors/timeEntries.ts
@@ -1,14 +1,8 @@
 import { createSelector } from 'reselect';
 
-const getTimeEntries = (state: any) => state.timeEntries.timeEntries;
-const getEditEntry = (state: any) => state.timeEntries.editEntry;
+const getTimeEntries = (state: any) => state.timeEntries;
 
 export const timeEntriesSelector = createSelector(
     [ getTimeEntries ],
     getTimeEntries => getTimeEntries
-);
-
-export const editEntrySelector = createSelector(
-    [ getEditEntry ],
-    getEditEntry => getEditEntry
 );
