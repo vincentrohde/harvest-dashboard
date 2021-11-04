9f4cec17b146af43902c5901fc3cce983bdae09b
vincentrohde <vincent.rohde@icloud.com>
2021-11-04 01:08:07 +0100

    update BackendService to handle direct requests to Harvest API

--- client/src/services/BackendService/BackendService.ts
+++ client/src/services/BackendService/BackendService.ts
@@ -1,53 +1,102 @@
 // Libs
 import axios from 'axios';
 
+// Services
+import oAuthService from '../OAuthService/OAuthService';
+
 // Types
-import { timeEntriesType, TimeEntryInterface, TimeEntrySubmissionInterface } from '@/types/TimeEntry';
-import { tasksType } from '@/types/Task';
-import { projectsType } from '@/types/Project';
+import {timeEntriesType, TimeEntryInterface, TimeEntrySubmissionInterface} from '@/types/TimeEntry';
+import {tasksType} from '@/types/Task';
+import {projectsType} from '@/types/Project';
+import {requestConfig} from './BackendService.types';
+
+const V2_HARVEST_API_URL = 'https://api.harvestapp.com' + '/v2';
+const TIME_ENTRIES_URL = V2_HARVEST_API_URL + '/time_entries';
+const TASKS_URL = V2_HARVEST_API_URL + '/tasks';
+const PROJECTS_URL = V2_HARVEST_API_URL + '/projects';
 
 class BackendService {
-    getTimeEntry (entryID: TimeEntryInterface['id']) {
-        return axios.get(`./api/time_entries/${entryID}`);
+    getAuthorizationConfig(write: boolean = false): Promise<requestConfig> {
+        return new Promise((resolve, reject) => {
+            const credentials = oAuthService.getOAuthCookieData();
+
+            if (credentials) {
+                const config: requestConfig = {
+                    headers: {
+                        Authorization: 'Bearer ' + credentials.access_token,
+                        'Harvest-Account-ID': credentials.account_id,
+                    },
+                };
+
+                if (write) {
+                    config.headers['Content-Type'] = 'application/json';
+                }
+
+                resolve(config);
+            } else {
+                reject('No Credentials. Please authorize.');
+            }
+        });
     }
 
-    getTimeEntries (from: string | undefined, to: string | undefined) {
+    getTimeEntries(from: string | undefined, to: string | undefined) {
         const isParams = typeof from !== 'undefined' && typeof to !== 'undefined';
         const queryString = isParams ? `?from=${from}&to=${to}` : '';
 
-        return axios.get('./api/time_entries' + queryString)
+        return this.getAuthorizationConfig()
+            .then((config) => {
+                return axios.get(TIME_ENTRIES_URL + queryString, config);
+            })
             .then((response) => {
-                const { timeEntries }: { timeEntries: timeEntriesType } = response.data;
+                const {timeEntries}: {timeEntries: timeEntriesType} = response.data;
                 return timeEntries;
             });
     }
 
-    getTasks () {
-        return axios.get('./api/tasks')
+    getTasks() {
+        return this.getAuthorizationConfig()
+            .then((config) => {
+                return axios.get(TASKS_URL, config);
+            })
             .then((response) => {
-                const { tasks }: { tasks: tasksType } = response.data;
+                const {tasks}: {tasks: tasksType} = response.data;
                 return tasks;
             });
     }
 
-    getProjects () {
-        return axios.get('./api/projects')
+    getProjects() {
+        return this.getAuthorizationConfig()
+            .then((config) => {
+                return axios.get(PROJECTS_URL, config);
+            })
             .then((response) => {
-                const { projects }: { projects: projectsType } = response.data;
+                const {projects}: {projects: projectsType} = response.data;
                 return projects;
             });
     }
 
-    addTimeEntry (timeEntry: TimeEntrySubmissionInterface) {
-        return axios.post('./api/time_entries', timeEntry);
+    getTimeEntry(entryID: TimeEntryInterface['id']) {
+        return this.getAuthorizationConfig().then((config) => {
+            return axios.get(`${TIME_ENTRIES_URL}/${entryID}`, config);
+        });
     }
 
-    updateTimeEntry (timeEntry: TimeEntrySubmissionInterface, entryID: TimeEntryInterface['id']) {
-        return axios.patch(`./api/time_entries/${entryID}`, timeEntry);
+    addTimeEntry(timeEntry: TimeEntrySubmissionInterface) {
+        return this.getAuthorizationConfig(true).then((config) => {
+            return axios.post(TIME_ENTRIES_URL, timeEntry, config);
+        });
     }
 
-    deleteTimeEntry (entryID: TimeEntryInterface['id']) {
-        return axios.delete(`./api/time_entries/${entryID}`);
+    updateTimeEntry(timeEntry: TimeEntrySubmissionInterface, entryID: TimeEntryInterface['id']) {
+        return this.getAuthorizationConfig(true).then((config) => {
+            return axios.patch(`${TIME_ENTRIES_URL}/${entryID}`, timeEntry, config);
+        });
+    }
+
+    deleteTimeEntry(entryID: TimeEntryInterface['id']) {
+        return this.getAuthorizationConfig(true).then((config) => {
+            return axios.delete(`${TIME_ENTRIES_URL}/${entryID}`, config);
+        });
     }
 }
 
