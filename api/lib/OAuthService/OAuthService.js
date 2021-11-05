b6745356d9ed67c166ce8a89024f91ab9978d466
vincentrohde <vincent.rohde@icloud.com>
2021-11-05 20:35:02 +0100

    add getOAuthData, getCredentials methods to api/OAuthService

--- api/lib/OAuthService/OAuthService.js
+++ api/lib/OAuthService/OAuthService.js
@@ -1,29 +1,12 @@
 const moment = require('moment');
-const expressErrorService = require('../ExpressErrorService/ExpressErrorService');
+
+const HARVEST_API_OAUTH_URL = process.env.HARVEST_API_OAUTH_URL;
+const OAUTH_APP_ID = process.env.OAUTH_APP_ID;
+const OAUTH_APP_SECRET = process.env.OAUTH_APP_SECRET;
+const VERIFICATION_SERVER_URL = process.env.VERIFICATION_SERVER_URL;
 
 // Service for handling everything related to OAuth Data
-
 class OAuthService {
-    handleHarvestRequest(req, res, success) {
-        const credentials = this.getOAuthCookieData(req);
-
-        if (credentials) {
-            const config = this.getRequestConfig(credentials);
-            success(config);
-        } else {
-            expressErrorService.sendReadOnlyErrorResponse(res);
-        }
-    }
-
-    getRequestConfig({access_token, account_id}) {
-        return {
-            headers: {
-                Authorization: 'Bearer ' + access_token,
-                'Harvest-Account-ID': account_id,
-            },
-        };
-    }
-
     getOAuthCookieConfig(expires_in) {
         return {
             maxAge: expires_in * 1000, // expires_in is a second value, while maxAge uses miliseconds
@@ -39,22 +22,34 @@ class OAuthService {
         });
     }
 
-    getOAuthCookieData(req) {
-        if (req.cookies.oauth) {
-            const parsedCookie = JSON.parse(req.cookies.oauth);
-            const {access_token, account_id} = parsedCookie;
-
-            return {
-                access_token,
-                account_id,
-            };
-        }
-
-        return false;
-    }
-
     getAccountId(scope) {
         return scope.split(':').pop().split(' ')[0];
+    }
+
+    // Redirection Server
+    getOAuthData(requestToken) {
+        return axios
+            .get(`${VERIFICATION_SERVER_URL}/oauth/data?requestToken=${requestToken}`)
+            .then((response) => {
+                const {data} = response;
+                return new Promise((resolve) => {
+                    resolve(data);
+                });
+            });
+    }
+
+    // Verification Server
+    getCredentials(requestToken) {
+        return axios
+            .post(
+                `${HARVEST_API_OAUTH_URL}?code=${requestToken}&client_id=${OAUTH_APP_ID}&client_secret=${OAUTH_APP_SECRET}&grant_type=authorization_code`,
+            )
+            .then((response) => {
+                const {data} = response;
+                return new Promise((resolve) => {
+                    resolve(data);
+                });
+            });
     }
 }
 
