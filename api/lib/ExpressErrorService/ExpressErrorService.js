8050c4e0d89ff4569c3a2f1720f708c3cba6cd6c
vincentrohde <vincent.rohde@icloud.com>
2021-11-05 20:27:32 +0100

    remove sendReadOnlyErrorResponse from api/ExpressErrorService

--- api/lib/ExpressErrorService/ExpressErrorService.js
+++ api/lib/ExpressErrorService/ExpressErrorService.js
@@ -1,20 +1,6 @@
 // Service for handling of Express.js errors
 
 class ExpressErrorService {
-    sendReadOnlyErrorResponse(res) {
-        const response = {
-            status: 401,
-            statusText: 'OAuth credentials are missing. Please authorize your application.',
-        };
-
-        this.sendErrorResponse(
-            {
-                response,
-            },
-            res,
-        );
-    }
-
     sendErrorResponse(error, res) {
         const {status: errorStatus, statusText: errorMessage} = error.response;
         res.status(errorStatus).send({
