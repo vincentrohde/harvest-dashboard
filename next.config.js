79d1f8821568fb25b3a85a667b9ff6b2348cd913
Vincent Rohde <vincent.rohde@icloud.com>
2022-04-13 00:56:37 +0200

    #61 - update next build script

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
     reactStrictMode: true,
+    assetPrefix: isProd ? './' : '',
     env: {
         HARVEST_API_URL: process.env.HARVEST_API_URL,
         OAUTH_APP_ID: process.env.OAUTH_APP_ID,
