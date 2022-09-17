310cf0ff99ea7f34c0da83304c19efc7b5c5188a
Vincent Rohde <vincent.rohde@icloud.com>
2022-09-17 23:09:51 +0200

    #67 - add MetaDataHeader component snapshot tests

---
+++ src/components/domain/TimeEntries/components/MetaDataHeader/MetaDataHeader.test.tsx
@@ -0,0 +1,25 @@
+// Libs
+import { render } from '@testing-library/react';
+import React from 'react';
+import '@testing-library/jest-dom/extend-expect';
+
+// Components
+import MetaDataHeader from './MetaDataHeader';
+
+// Types
+import { MetaDataHeaderProps } from './MetaDataHeader.types';
+
+const mockData: MetaDataHeaderProps = {
+    entriesAmount: 20,
+    totalHoursAndMinutes: '2:00'
+}
+
+describe('<MetaDataHeader />', () => {
+    it('renders', () => {
+        const { asFragment } = render(
+            <MetaDataHeader {...mockData} />
+        );
+
+        expect(asFragment()).toMatchSnapshot();
+    });
+})
