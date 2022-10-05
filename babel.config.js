4fdac1fd585225130c7f18289008ba2ce03dc82a
Vincent Rohde <vincent.rohde@icloud.com>
2022-10-05 23:17:54 +0200

    #67 - update babel plugins

--- babel.config.js
+++ babel.config.js
@@ -4,4 +4,10 @@ module.exports = {
         '@babel/preset-react',
         '@babel/preset-typescript'
     ],
+    plugins: [
+        ['@babel/plugin-transform-runtime', {regenerator: true }],
+        '@babel/plugin-proposal-class-properties',
+        '@babel/plugin-proposal-export-default-from',
+        ['@babel/plugin-transform-react-jsx', { pragma:'h' }]
+    ]
 };
