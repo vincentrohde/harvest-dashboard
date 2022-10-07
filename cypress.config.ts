8ff115cf80b7c4f3c9ab5f58af8cef0c3f87929f
Vincent Rohde <vincent.rohde@icloud.com>
2022-10-07 08:07:01 +0200

    #67 - refactor cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,12 +1,6 @@
 import {defineConfig} from 'cypress';
 
 export default defineConfig({
-    e2e: {
-        // setupNodeEvents (on, config) {
-        //     // implement node event listeners here
-        // },
-    },
-
     component: {
         devServer: {
             framework: 'next',
